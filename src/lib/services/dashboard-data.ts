3157a744134d743909a2ce79395e3dbe6d5a7b50
ABDUL BASIT <absh5506@gmail.com>
2026-06-25 09:33:14 +0500

    Add dashboard data service barrel

---
+++ src/lib/services/dashboard-data.ts
@@ -0,0 +1,15 @@
+export type {
+  DashboardAuditEvent,
+  DashboardDataService,
+  DashboardDevice,
+  DashboardDeviceStatus,
+  DashboardListOptions,
+  DashboardSession,
+  DashboardSessionStatus,
+} from "./dashboard-contracts";
+
+export {
+  mapDashboardAuditEvent,
+  mapDashboardDevice,
+  mapDashboardSession,
+} from "./dashboard-mappers";
